1f69f25c24bc70754fec8ada95c2722c6d6862ea
Dinesh <dinesh.s@ictgroup.ind.in>
2020-06-14 17:22:22 +0530

    Code Structure improved in provider

--- count_provider/lib/main.dart
+++ count_provider/lib/main.dart
@@ -5,27 +5,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider<CountProvider>(
+      create: (context) => CountProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<CountProvider>(
-      create: (context) => CountProvider(),
-      child: Builder(
-        builder: (context) {
-          return MaterialApp(
-            title: "Count",
-            initialRoute: '/add_count',
-            routes: {
-              '/add_count': (context) => AddCount(),
-              '/remove_count': (context) => RemoveCount()
-            },
-          );
-        },
-      ),
+    return MaterialApp(
+      title: "Count",
+      initialRoute: '/add_count',
+      routes: {
+        '/add_count': (context) => AddCount(),
+        '/remove_count': (context) => RemoveCount()
+      },
     );
   }
 }
